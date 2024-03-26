94bb72af4bdf7dbbfe6a89323806fdee6c8abe8f
Tus99 <nguyentu12aa4@gmail.com>
2024-03-26 19:47:37 +0700

    init project

---
+++ ReviewFood/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using ReviewFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReviewFood.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        dbReviewFoodDataContext db = new dbReviewFoodDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            var all_key = from r in db.Posts select r;
+            return View(all_key);
+        }
+    }
+}
